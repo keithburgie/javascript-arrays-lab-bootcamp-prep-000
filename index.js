2a8194d21492c6e62e5b800b5eb5701da49278bc
Keith Burgie <krburgie@gmail.com>
2019-04-16 04:06:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,26 +23,6 @@ function prependKitten(name) {
   var array = [name, ...kittens]
   return array;
 }
-/*
-  describe('appendKitten(name)', function() {
-    it('appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      window.kittens = ['Milo', 'Otis', 'Garfield'];
-      expect(appendKitten("Broom")).toEqual(["Milo", "Otis", "Garfield", "Broom"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
-
-  describe('prependKitten(name)', function() {
-    it('prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      window.kittens = ['Milo', 'Otis', 'Garfield'];
-      expect(prependKitten("Arnold")).toEqual(["Arnold", "Milo", "Otis", "Garfield"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
-*/
-
 
 function removeLastKitten(array) {
   array = kittens.slice(0, kittens.length - 1);
